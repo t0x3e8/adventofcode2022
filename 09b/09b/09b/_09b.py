69a59d87dddda67a90d7585f1c89f89438daf3f7
Jarek <t0x3e8@gmail.com>
2022-12-10 02:01:59 +0100

    09a done

---
+++ 09b/09b/09b/_09b.py
@@ -0,0 +1,40 @@
+import math
+
+# (x, y)
+fContent = open("testinput.txt", "r").readlines()
+
+def dist(p1, p2):
+    return math.hypot(p2[0] - p1[0], p2[1] - p1[1])
+
+def add(p1, p2):
+    return (p1[0] + p2[0], p1[1] + p2[1])
+
+def moveH(h, direction):  
+    if direction == "R": return add(h, (1, 0))
+    elif direction == "L": return add(h, (-1, 0))
+    elif direction == "U": return add(h, (0, -1))
+    else: return add(h, (0, 1))
+
+def moveT(h, t, direction):
+    if direction == "R": return (h[0] - 1, h[1])
+    elif direction == "L": return (h[0] + 1, h[1])
+    elif direction == "U": return (h[0], h[1]  + 1)
+    else: return (h[0], h[1] - 1)
+    
+h = (0, 0)
+ropes = [(0, 0), (0, 0)]
+fields = []
+for line in fContent:
+    directory, length = line.strip().split(" ")
+    print(h, "-----------", line.strip())
+    for i in range(len(ropes)):
+        for j in range(0, int(length)):
+            h = moveH(h, directory)
+            distance = dist(h, r)
+            if distance >= 2:
+                t = moveT(h, r, directory)
+                fields.append(t)
+            print("h:", h, "t:", t, "dist:", distance)
+
+
+print("Positions visited by tail of the rope: ", len(dict.fromkeys(fields)) + 1)

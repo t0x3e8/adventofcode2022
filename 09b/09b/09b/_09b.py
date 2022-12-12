c0d62fd36d35256466b31b286be15e2a3a326267
Jarek <t0x3e8@gmail.com>
2022-12-12 17:40:20 +0100

    09b done

--- 09b/09b/09b/_09b.py
+++ 09b/09b/09b/_09b.py
@@ -1,55 +1,29 @@
 import math
 import numpy as np
 
-# (x, y)
-fContent = open("testinput.txt", "r").readlines()
+fContent = open("input.txt", "r").readlines()
+directions = {"L": (-1, 0), "R": (1, 0), "U": (0, -1), "D": (0, 1)}
+ropes = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+fields = [[], [], [], [], [], [], [], [], []]
 
 def dist(p1, p2):
     return math.hypot(p2[0] - p1[0], p2[1] - p1[1])
 
-def add(p1, p2):
-    return (p1[0] + p2[0], p1[1] + p2[1])
+def calc(p1, p2):
+    diff = (p1[0] - p2[0], p1[1] - p2[1])
+    return (p1[0] - np.sign(diff[0]), p1[1] - np.sign(diff[1]))
 
-def calc(p1, p2, d):
-    # U D
-    if d[0] == 0:
-        diff = abs(p1[0]) - abs(p2[0])
-        if (p1[0] >= 0):
-            p1 = (p1[0] - np.sign(diff), p1[1] + np.sign(d[1]))
-        if (p1[0] <= 0):
-            p1 = (p1[0] + np.sign(diff), p1[1] - np.sign(d[1]))
-    # L R
-    if d[1] == 0:
-        diff = abs(p1[1]) - abs(p2[1])
-        if (p1[1] >= 0):
-            p1 = (p1[0] + np.sign(d[0]), p1[1] - np.sign(diff))  
-        if (p1[1] < 0):
-            p1 = (p1[0] - np.sign(d[0]), p1[1] + np.sign(diff))
-        
-
-    return p1
-
-
-directions = {"L": (-1, 0), "R": (1, 0), "U": (0, -1), "D": (0, 1)}
-ropes = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
-fields = []
 for line in fContent:
     d, length = line.strip().split(" ")
-    print(ropes[0], "-----------", line.strip())
         
     for i in range(int(length)):
-        ropes[0] = add(ropes[0], directions[d])
-        print(f"--> 0: {ropes[0]}")
+        ropes[0] = (ropes[0][0] + directions[d][0], ropes[0][1] + directions[d][1])
 
         for ropeIndex in range(1, len(ropes)):
             rng = dist(ropes[ropeIndex], ropes[ropeIndex - 1])
-            if rng >= 2:
-                s = (f"{ropeIndex}: OLD {ropes[ropeIndex]}, DIR: {directions[d]}, DIFF: ({ropes[ropeIndex][0] - ropes[ropeIndex - 1][0]}, {ropes[ropeIndex][1] - ropes[ropeIndex - 1][1]})")
-                ropes[ropeIndex] = calc(ropes[ropeIndex], ropes[ropeIndex - 1], directions[d])
-                fields.append(ropes[ropeIndex])                
-                print(s + " NEW: ", ropes[ropeIndex])
-            else:
-                print(f"    {ropeIndex}: {ropes[ropeIndex]}")
+            if rng >= 2 :
+                ropes[ropeIndex] = calc(ropes[ropeIndex], ropes[ropeIndex - 1])
+                fields[ropeIndex - 1].append(ropes[ropeIndex])
 
-print("Positions visited by tail of the 9 ropes: ", len(dict.fromkeys(fields)) + 1)
-#2471
+for i in range(9):
+    print(f"Positions visited the tail of {i} rope: ", len(dict.fromkeys(fields[i])) + 1)

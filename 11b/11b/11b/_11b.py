4cc57c94b71da3cb7b3418bfc15718d406cd4153
Jarek <t0x3e8@gmail.com>
2022-12-13 22:18:39 +0100

    11b done

--- 11b/11b/11b/_11b.py
+++ 11b/11b/11b/_11b.py
@@ -1,8 +1,10 @@
-import re, time
-import re, time
+import re, time, math
 from collections import deque
 
-fContent = open("testinput.txt", "r").read()
+def add(a, b): return a + b
+def multiply(a, b): return a * b
+
+fContent = open("input.txt", "r").read()
 monkeys = {}
 
 mSet = [re.findall(r"Starting items: (.*)", fContent),
@@ -11,41 +13,34 @@ mSet = [re.findall(r"Starting items: (.*)", fContent),
            re.findall(r"If true: throw to monkey (.*)", fContent),
            re.findall(r"If false: throw to monkey (.*)", fContent)]
 
-monkeys = {
-    "0": {"items": deque(re.findall(r"\d+", mSet[0][0])),
-           "op": mSet[1][0],
-           "test": int(re.findall(r"\d+", mSet[2][0])[0]),
-           "true": int(re.findall(r"\d+", mSet[3][0])[0]),
-           "false":  int(re.findall(r"\d+", mSet[4][0])[0]),
-           "count": 0},
-    "1": {"items": deque(re.findall(r"\d+", mSet[0][1])),
-           "op": mSet[1][1],
-           "test": int(re.findall(r"\d+", mSet[2][1])[0]),
-           "true": int(re.findall(r"\d+", mSet[3][1])[0]),
-           "false":  int(re.findall(r"\d+", mSet[4][1])[0]),
-           "count": 0},
-    "2": {"items": deque(re.findall(r"\d+", mSet[0][2])),
-           "op": mSet[1][2],
-           "test": int(re.findall(r"\d+", mSet[2][2])[0]),
-           "true": int(re.findall(r"\d+", mSet[3][2])[0]),
-           "false":  int(re.findall(r"\d+", mSet[4][2])[0]),
-           "count": 0},
-    "3": {"items": deque(re.findall(r"\d+", mSet[0][3])),
-           "op": mSet[1][3],
-           "test": int(re.findall(r"\d+", mSet[2][3])[0]),
-           "true": re.findall(r"\d+", mSet[3][3])[0],
-           "false":  re.findall(r"\d+", mSet[4][3])[0],
-           "count": 0},
-            }
-
+for i in range(len(mSet[0])):
+    monkeys[str(i)] = {"items": deque(re.findall(r"\d+", mSet[0][i])),
+               "op": mSet[1][i],
+               "test": int(re.findall(r"\d+", mSet[2][i])[0]),
+               "true": int(re.findall(r"\d+", mSet[3][i])[0]),
+               "false":  int(re.findall(r"\d+", mSet[4][i])[0]),
+               "count": 0}
+          
+M = math.prod(int (x) for x in mSet[2])
 start_time = time.time()
-for r in range(400):
+for r in range(10000):
     for k, v in monkeys.items():
         while len(v["items"]) > 0:
             item = v["items"].pop()
             v["count"] += 1
-            wl = eval(v["op"].replace("old", str(item)))
-            test = wl % v["test"] == 0
+            #wl = eval(v["op"].replace("old", str(item)))
+            
+            args = v["op"].split(" ")
+            args[0] = int(item) if args[0] == "old" else int(args[0])
+            args[2] = int(item) if args[2] == "old" else int(args[2])
+            wl = int()
+            if "*" == args[1]:
+                wl = multiply(args[0], args[2])
+            elif "+" == args[1]:
+                wl = add(args[0], args[2])
+
+            wl = wl % M
+            test = wl % (v["test"]) == 0
             if test:
                 monkeys[str(v["true"])]["items"].append(wl)
             else:
@@ -54,7 +49,10 @@ for r in range(400):
 end_time = time.time()
 elapsed_time = end_time - start_time
 print('The operation took {} seconds to complete.'.format(elapsed_time))
-           
+
 for k, v in monkeys.items():
     print(f"Monkey {k} inspected items {v['count']} times.")
+
+list = list(sorted(monkeys.items(), key = lambda x: x[1]["count"], reverse=1)[:2])
+print(f"Tatal is {list[0][1]['count']} * {list[1][1]['count']} = {list[0][1]['count'] * list[1][1]['count']}")   
     

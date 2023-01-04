5ea255cfc3d085746b7553ab168d865b9f8324b2
Jarek <t0x3e8@gmail.com>
2023-01-04 18:29:05 +0100

    ToDo: Improve performance

--- 15a/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ 15a/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -2,6 +2,7 @@ import re
 import numpy as np
 import math
 import time
+from multiprocessing import Pool
 
 def flatten(array):
     newArr = []
@@ -21,49 +22,43 @@ def read(fileName):
 
         return pairs
     
-data = read("testinput.txt")
+def markUsedPosition(data, minX, y):
+    arr = []
+    #mark beacons and sensons
+    for sb in data:
+        if sb[1][0] == y or sb[0][0] == y:
+            arr.append(abs(minX) + sb[1][1])
+            break
+    return arr
+
+data = read("input.txt")
 xs = np.array([[el[0][1] - el[2], el[1][1], el[0][1] + el[2]] for el in data]).flatten()
 minX = min(xs)
 maxX = max(xs)
 width = maxX - minX
-#y = 11
+#y = 10
 y = 2000000
 
 sTime = time.time()
-arr = ["#"] * width
-usedPositions = []
-#mark beacons and sensons
-for sb in data:
-    if sb[1][0] == y or sb[0][0] == y:
-        usedPositions.append(abs(minX) + sb[1][1])
-        break
+usedPositions = markUsedPosition(data, minX, y)
 eTime = time.time()
 print(f"Marking B and S time: {(eTime-sTime):.5f}")
 
 sTime = time.time()
+count = 0
 for x in range(minX, width + minX):
-    inRange = False
+    if x in usedPositions:
+        continue
     
     for sb in data:
         sbDist = sb[2]
         sxDist = abs(sb[0][0] - y) + abs(sb[0][1] - x)   # sensor-(x, y) distance
 
         if sbDist >= sxDist: 
-            inRange = True
+            count += 1
             break
-    
-    if not inRange:
-        arr[x - minX] = "."
-        
 eTime = time.time()
 print(f"Calculating used positions time: {(eTime-sTime):.5f}")
 
-sTime = time.time()
-count = 0 
-for a in arr:
-    if a == "#":
-        count+=1
-eTime = time.time()
-print(f"Counting time: {(eTime-sTime):.5f}")
 print(f"In the row where y={y}, {count} positions cannot contain a beacon ")
 # 6078701

61f525f1930c991086ed7a75613e52a1aab0136f
Jarek <t0x3e8@gmail.com>
2023-01-04 17:50:17 +0100

    15a Done

--- 15a/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ 15a/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,6 +1,7 @@
 import re
 import numpy as np
 import math
+import time
 
 def flatten(array):
     newArr = []
@@ -21,28 +22,31 @@ def read(fileName):
         return pairs
     
 data = read("testinput.txt")
-xs = np.array([[el[0][1], el[1][1]] for el in data]).flatten()
+xs = np.array([[el[0][1] - el[2], el[1][1], el[0][1] + el[2]] for el in data]).flatten()
 minX = min(xs)
 maxX = max(xs)
 width = maxX - minX
-y = 11   
+#y = 11
+y = 2000000
 
+sTime = time.time()
 arr = ["#"] * width
+usedPositions = []
 #mark beacons and sensons
 for sb in data:
-    if sb[1][0] == y:
-        arr[abs(minX) + sb[1][1]] = "B"
+    if sb[1][0] == y or sb[0][0] == y:
+        usedPositions.append(abs(minX) + sb[1][1])
         break
-    if sb[0][0] == y:
-        arr[abs(minX) + sb[0][1]] = "S"
-        break
+eTime = time.time()
+print(f"Marking B and S time: {(eTime-sTime):.5f}")
 
+sTime = time.time()
 for x in range(minX, width + minX):
     inRange = False
     
     for sb in data:
         sbDist = sb[2]
-        sxDist = abs(sb[0][0] - y) + abs(sb[0][1] - x)                      # sensor-(x, y) distance
+        sxDist = abs(sb[0][0] - y) + abs(sb[0][1] - x)   # sensor-(x, y) distance
 
         if sbDist >= sxDist: 
             inRange = True
@@ -50,11 +54,16 @@ for x in range(minX, width + minX):
     
     if not inRange:
         arr[x - minX] = "."
+        
+eTime = time.time()
+print(f"Calculating used positions time: {(eTime-sTime):.5f}")
 
+sTime = time.time()
 count = 0 
 for a in arr:
     if a == "#":
         count+=1
-print(arr)
+eTime = time.time()
+print(f"Counting time: {(eTime-sTime):.5f}")
 print(f"In the row where y={y}, {count} positions cannot contain a beacon ")
-# 3876819
+# 6078701

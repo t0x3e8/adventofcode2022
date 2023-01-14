eca8e249b7dcbafed0ae0fcff65cca0f0593e454
Jarek <t0x3e8@gmail.com>
2023-01-14 17:12:27 +0100

    14b done

--- 14b/14b/14b/_14b.py
+++ 14b/14b/14b/_14b.py
@@ -1,28 +1,19 @@
-
 import re
 import numpy as np
-fContent = open("testinput.txt", "r").read()
+fContent = open("input.txt", "r").read()
 
-
-#b = np.eye(10)
-#c = np.empty((10, 1))
-#b = np.insert(b, 0, c, axis=1)
-
-def ppp(b):
-    for i in range(len(b)):
-        for j in range(len(b[i])):
-            print(b[i][j], " ", end="")
-        print("")
-
+def printCave(cave):
+    for l in cave:
+        print("".join(l))
 
 pairs = re.findall(r"(\d+,\d+)", fContent, re.DOTALL)
 xSet = [int(x.split(",")[0]) for x in pairs]
 ySet = [int(x.split(",")[1]) for x in pairs]
-shape = (max(ySet) + 1, max(xSet) - min(xSet) + 1)
+shape = (max(ySet) + 3, 2 * (max(ySet) + 3) + 1)
 cave = np.full(shape, ".")
-cave[0, 500 - min(xSet)] = "+"
+cave[0][int(shape[1] / 2)] = "+"
+cave[shape[0] - 1] = "#" * shape[1]
 
-offset = (min(xSet), min(ySet))
 for line in fContent.split("\n"):
     pairs2 = re.findall(r"(\d+,\d+)+", line, re.DOTALL)
     for i in range(len(pairs2)):
@@ -31,38 +22,23 @@ for line in fContent.split("\n"):
         pNext = pairs2[i + 1].split(",")
         for x in range(min(int(pCur[0]), int(pNext[0])), max(int(pCur[0]), int(pNext[0])) + 1):
             for y in range(min(int(pCur[1]), int(pNext[1])), max(int(pCur[1]), int(pNext[1])) + 1):
-                cave[y][x-offset[0]] = "#"
+                offset = x - 500 + int(shape[1] / 2)
+                cave[y][offset] = "#"
 
-sand = (1, 500 - min(xSet))
-x = 0
-y = 1
+sand = (0, int(shape[1] / 2))
+x, y = (0, 0)
+
 while True:
-    ppp(cave)
-    print(x)
-    if (sand[1] + x) < 0:
-        newC = np.ones((1, max(ySet) + 1))
-        cave = np.insert(cave, 0, newC, axis = 1)
-        cave[len(cave) - 1][0] = "#"
-        x = 0 
-        y = 1
-    if (sand[1] + x) >= len(cave[0]):
-        newC = np.ones((1, max(ySet) + 1))
-        cave = np.insert(cave, len(cave[0]), newC, axis = 1)
-        cave[len(cave) - 1][max(ySet) + 1] = "#"
-        x = 0 
-        y = 1
-    field = cave[sand[0] + y][sand[1] + x]
-    if field == ".":
-        y += 1
+    for yy, xx in [(1, 0), (1, -1), (1, 1)]:
+            if cave[sand[0] + y + yy][sand[1] + x + xx] == ".":
+                y += yy
+                x += xx
+                break
     else:
-        x -= 1
-        field = cave[sand[0] + y][sand[1] + x]
-        if not field == ".":
-            x += 2
-            field = cave[sand[0] + y][sand[1] + x]
-            if not field == ".":
-                cave[sand[0] + y - 1][sand[1] + x - 1] = "o"
-                x = 0
-                y = 1
-                                
-print("Total number of 'o' is: ", np.count_nonzero(cave == "o"))
+        cave[sand[0] + y][sand[1] + x] = "O"
+        if x == 0 and y == 0:
+            break
+        x, y = (0, 0)
+
+printCave(cave)
+print("Total number of 'O' is: ", np.count_nonzero(cave == "O"))
